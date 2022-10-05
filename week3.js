445f81a789a4475aedc4034f0b381dd5a99fa001
shinoshyun <8367shyun@gmail.com>
2022-10-05 18:38:57 +0800

    Update week3.js

--- week3.js
+++ week3.js
@@ -1,16 +1,23 @@
 fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then(function (response) {
     return response.json();
 }).then(function (data) {
-    let data2 = data.result.results.slice(0, 2)
-    for (let i = 0; i <= data2.length; i++) {
+    //把json資料取出 data2
+    let data2 = data.result.results
+    //i<2 取到前兩筆放入information內
+    for (let i = 0; i < 2; i++) {
         let information = data2[i];
 
+        // 創一個<h3></h3>
         let h3 = document.createElement("h3");
+        //再創一個 取出json資料內的stilte(景點)
         let stitle = document.createTextNode(information.stitle);
+        //把stitle放進h3內
         h3.appendChild(stitle);
-        console.log(h3);
 
+        //找到HTML中 item1的位置
         let element = document.querySelector(".item1");
+
+        //把剛剛取出的h3再放進去
         element.appendChild(h3);
     }
 
