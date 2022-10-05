a342a46dcb8d88c4079c364d91d27771eaba17c1
shinoshyun <8367shyun@gmail.com>
2022-10-05 16:34:16 +0800

    Update week3.js

--- week3.js
+++ week3.js
@@ -4,14 +4,14 @@ fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assi
     let data2 = data.result.results
     let item1 = document.querySelector("#item1");
     for (let i = 0; i < data2.length; i++) {
-        let product = data2[i];
-        let h2 = document.createElement("h2")
-        let stitle = document.createTextNode(product.stitle)
-        h2.appendChild(stitle)
+        let information = data2[i];
+        let h2 = document.createElement("h2");
+        let stitle = document.createTextNode(information.stitle);
+        h2.appendChild(stitle);
         console.log(h2);
 
-        let element = document.getElementById()
-        element.appendChild(stitle)
+        // let element = document.getElementById('title');
+        // element.appendChild("h2");
     }
 
 })
