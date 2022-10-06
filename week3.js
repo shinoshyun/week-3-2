6349b6375e22db0ab6c03a119cd55a83f2f8af6a
shinoshyun <8367shyun@gmail.com>
2022-10-06 16:27:47 +0800

    Update week3.js

--- week3.js
+++ week3.js
@@ -4,19 +4,14 @@ fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assi
     //把json資料取出 data2
     let data2 = data.result.results
 
-
     //i<2 取到前兩筆放入information內
     for (let i = 0; i < 2; i++) {
         let information = data2[i].stitle;
 
-        let jpg = data2[i].file.split(".jpg")[0] + ".jpg"
+        let jpg = data2[i].file.replace(/jpg/gi, "jpg").split(".jpg")[0] + ".jpg";
         let imgSrc = document.createElement("img");
         imgSrc.setAttribute("src", jpg);
-        let file = document.createTextNode(jpg);
-        imgSrc.appendChild(file);
 
-        let pic = document.querySelectorAll(".item1");
-        pic[i].appendChild(imgSrc);
 
         // 創一個<h3></h3>  //再創一個 取出json資料內的stilte(景點)  //把stitle放進h3內
         let h3 = document.createElement("h3");
@@ -24,22 +19,18 @@ fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assi
         h3.appendChild(stitle);
         //找到HTML item1的位置  //依序把h3放入
         let element = document.querySelectorAll(".item1");
+        element[i].appendChild(imgSrc);
         element[i].appendChild(h3);
 
     }
 
 
-    for (i = 2; i < data2.length; i++) {
+    for (i = 2; i < 10; i++) {
         let information = data2[i].stitle;
 
-        let jpg = data2[i].file.split(".jpg")[0] + ".jpg"
+        let jpg = data2[i].file.replace(/jpg /gi, "jpg").split(".jpg")[0] + ".jpg";
         let imgSrc = document.createElement("img");
         imgSrc.setAttribute("src", jpg);
-        let file = document.createTextNode(jpg);
-        imgSrc.appendChild(file);
-
-        let pic = document.querySelectorAll(".title");
-        pic[i - 2].appendChild(imgSrc);
 
 
         let h2 = document.createElement("h2");
@@ -48,8 +39,8 @@ fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assi
 
 
         let elements = document.querySelectorAll(".title");
+        elements[i - 2].appendChild(imgSrc);
         elements[i - 2].appendChild(h2);
-
 
     }
 })
