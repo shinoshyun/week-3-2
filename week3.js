df6b216be64c0621e8cccf8f8395e34b76a732e0
shinoshyun <8367shyun@gmail.com>
2022-10-06 14:03:39 +0800

    Update week3.js

--- week3.js
+++ week3.js
@@ -3,22 +3,39 @@ fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assi
 }).then(function (data) {
     //把json資料取出 data2
     let data2 = data.result.results
-    //i<2 取到前兩筆放入information內
-    for (let i = 0; i < 2; i++) {
-        let information = data2[i];
+    console.log(data2)
+        for (let i = 0; i < data2.length; i++) {
+            let information = data2[i];
 
-        // 創一個<h3></h3>
-        let h3 = document.createElement("h3");
-        //再創一個 取出json資料內的stilte(景點)
-        let stitle = document.createTextNode(information.stitle);
-        //把stitle放進h3內
-        h3.appendChild(stitle);
+            // 創一個<h3></h3>  //再創一個 取出json資料內的stilte(景點)  //把stitle放進h3內
+            let h3 = document.createElement("h3");
+            let stitle = document.createTextNode(information.stitle);
+            h3.appendChild(stitle);
+            //找到HTML item1的位置  //依序把h3放入
+            let element = document.querySelectorAll(".item1");
+            element[i].appendChild(h3);
 
-        //找到HTML中 item的位置，把item變成一個變數
-        let element = document.querySelector(`.item${i + 1}`);
+            let jpg = information.file.split(".jpg")[0] + ".jpg"
+            let imgSrc = document.createElement("img");
+            imgSrc.setAttribute("src", jpg);
+            let file = document.createTextNode(jpg);
+            imgSrc.appendChild(file);
 
-        //把剛剛取出的h3再放進去
-        element.appendChild(h3);
-    }
+            let pic = document.querySelectorAll(".item1");
+            element[i].appendChild(imgSrc);
+        }
 
+
+        // for (let i = 2; i < data2.length; i++) {
+        //     let information = data2[i];
+        //     let h2 = document.createElement("h2");
+        //     let stitle = document.createTextNode(information.stitle);
+        //     h2.appendChild(stitle);
+        //     console.log(h2);
+
+        //     let elements = document.querySelectorAll(".title");
+        //     elements[i].appendChild(h2);
+
+
+        // }
 })
